eb770e307e912bb2b91730deda93dd3e3a239f49
BigECire <ericsanders5800@gmail.com>
2019-06-13 19:24:38 -0500

    fixed the other seaches not working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,8 +5,8 @@ console.log("working")
 
 var searchType = $(".type-of-search").val();
 var seachTerm = $(".search-term").val();
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?" + searchType + "=" + seachTerm;
-
+var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/" + searchType + "=" + seachTerm;
+// var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list"
     $.ajax({
       url: queryURL,
       method: "GET"
