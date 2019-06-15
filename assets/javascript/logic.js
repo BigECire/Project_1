828e92c7561f9d737ab83e0552f5ffd75484742c
BigECire <ericsanders5800@gmail.com>
2019-06-15 09:43:38 -0500

    added second on click

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,20 +1,21 @@
-$( document ).ready(function() {
-$("#submit").on("click", function(){
-    
-console.log("working")
+$(document).ready(function () {
+  $("#submit").on("click", function () {
 
-var searchType = $(".type-of-search").val();
-var seachTerm = $(".search-term").val();
-var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/" + searchType + "=" + seachTerm;
-// var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list"
+    console.log("working")
+
+    var searchType = $(".type-of-search").val();
+    var seachTerm = $(".search-term").val();
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/" + searchType + "=" + seachTerm;
+    // var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list"
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
       for (let i = 0; i < response.drinks.length; i++) {
         var card = $("<div>")
         card.addClass("card result-card")
+        card.attr("data-id", response.drinks[i].idDrink)
 
         var image = $("<img>")
         image.addClass("card-img-top")
@@ -28,11 +29,18 @@ var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/" + searchType + "="
         card.append(title)
 
         $("#results").append(card)
-        
+
       }
     });
-});
-$(document).on("click", "result-card", function() {
-  
-})
+  });
+  $(document).on("click", ".result-card", function () {
+    console.log("clicked");
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + $(this).attr("data-id")
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    })
+  })
 })
