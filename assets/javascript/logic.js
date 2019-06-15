aa387941cffd71202ad102b07e04eec36e466186
BigECire <ericsanders5800@gmail.com>
2019-06-15 12:48:11 -0500

    got the recipe on recipe page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -31,27 +31,54 @@ $(document).ready(function () {
         $("#results").append(card)
 
       }
+      var URL = "https://calendarific.com/api/v2?api_key=2c0dbaf7a60237e5add4aeb4980344a79a0d269d&country=US&year=2019"
+      $.ajax({
+        url: URL,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+      })
     });
   });
   $(document).on("click", ".result-card", function () {
     console.log("clicked");
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + $(this).attr("data-id")
+
+    var drinkId = $(this).attr("data-id")
+    localStorage.clear();
+    localStorage.setItem("Id", drinkId);
+    document.location.href = "recipe_page.html"
+    
+    
+  })
+
+  var currentDrink = localStorage.getItem("Id")
+  if(!isNaN(currentDrink)){
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + localStorage.getItem("Id")
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
       console.log(response);
+      
       console.log(response.drinks[0].strDrink);
+      $("#recipe-name").text(response.drinks[0].strDrink)
       console.log(response.drinks[0].strDrinkThumb);
+      $("#recipe-img").attr("src", response.drinks[0].strDrinkThumb)
       console.log(response.drinks[0].strInstructions);
+      $("#recipe-instructions").text(response.drinks[0].strInstructions)
       var ingredient = response.drinks[0].strIngredient1
-        var i = 1
+      var i = 1
       while (ingredient !== "") {
         console.log(response["drinks"][0]["strIngredient" + i] + ": " + response["drinks"][0]["strMeasure" + i]);
+        var newRow = $("<tr>").append(
+          $("<td>").text(response["drinks"][0]["strIngredient" + i]),
+          $("<td>").text(response["drinks"][0]["strMeasure" + i])
+        );
+
+        $("#ingredients-table").append(newRow);
         i++
         ingredient = response["drinks"][0]["strIngredient" + i]
       }
-      
-    })
-  })
+
+    })}
 })
